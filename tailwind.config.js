e982d7acbe7279a53ae7058ee344be814bc40249
ion <>
2025-08-01 17:52:05 +0800

    init

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./components/**/*.{js,vue,ts,pug}",
+    "./layouts/**/*.{js,vue,ts,pug}",
+    "./pages/**/*.{js,vue,ts,pug}",
+    "./plugins/**/*.{js,ts}",
+    "./app.vue",
+    "./error.vue",
+  ],
+  theme: {},
+  plugins: [],
+}
+
