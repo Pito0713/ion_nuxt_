51e74599ca586f7a4f64a0553d4a0ef3d0111a8b
Liang Aoi <58981867+Pito0713@users.noreply.github.com>
2025-08-05 15:59:12 +0800

    專案架構建設導入 codemode & usefetch

---
+++ pages/main/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>    
+        <a class="text-3xl font-bold underline">
+    {{ data.data.uuid }}
+        </a>
+          <select v-model="$colorMode.preference">
+      <option value="system">System</option>
+      <option value="light">Light</option>
+      <option value="dark">Dark</option>
+      <option value="sepia">Sepia</option>
+    </select>
+<NuxtLink to="/single">single page</NuxtLink>
+    </div>
+    
+    
+
+</template>
+
+<script setup>
+// 取得 runtimeConfig
+const colorMode = useColorMode()
+console.log(colorMode.preference)
+const { data } = await useApiFetch('/users/3b1a0a83-d329-42af-8270-5168c7594237')
+</script>
---
+++ pages/single/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>    
+        <a class="text-3xl font-bold underline">
+          single page
+        </a>
+        <NuxtLink to="/main">main page</NuxtLink>
+    </div>
+
+</template>
+
+<script setup>
+</script>
