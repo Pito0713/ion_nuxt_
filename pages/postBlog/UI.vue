0811a261445cc801ea564e27a51c341807945069
Liang Aoi <58981867+Pito0713@users.noreply.github.com>
2025-09-09 17:45:38 +0800

    調整 UI 與子件拆分

--- pages/postBlog/UI.vue
+++ pages/postBlog/UI.vue
@@ -1,19 +1,14 @@
 <script setup lang="ts">
-import { ref } from 'vue'
 import TiptapEditor from '~/components/common/TiptapEditor.vue'
-import UIModal from '~/components/common/UIModal.vue'
 import type { InputMenuItem } from '@nuxt/ui'
+import { useHook } from './useHook'
+import AddTagModal from './AddTagModal.vue'
 import { z } from 'zod'
-
-const emit = defineEmits<{
-  (e: 'create-tags' | 'form-submit'): void
-  (e: 'update:addTagValue', value: string): void
-  (e: 'update:form', value: { title: string, content: string, tag: InputMenuItem }): void
-}>()
-
+// ----------------------------------------------------
+// props
+// ----------------------------------------------------
 const props = defineProps<{
   tagItems: InputMenuItem[],
-  addTagValue: string
   form: {
     tag: string,
     title: string,
@@ -21,47 +16,43 @@ const props = defineProps<{
   }
 }>()
 
-const showModal = ref(false)
+// ----------------------------------------------------
+// emit
+// ----------------------------------------------------
+const emit = defineEmits<{
+  (e: 'create-tags' | 'form-submit'): void
+  (e: 'update:form', value: { title: string, content: string, tag: InputMenuItem }): void
+}>()
+
+// ----------------------------------------------------
+// config
+// ----------------------------------------------------
 const schema = z.object({
   tag: z.string().nonempty('請選擇狀態'),
   title: z.string().nonempty('標題必填'),
 })
 
-const create_tags = () => {
-  emit('create-tags')
-  showModal.value = false
-}
+// ----------------------------------------------------
+// Hook
+// ----------------------------------------------------
+const { addTagValue, createTags, } = useHook()
 </script>
 
 <template>
   <LayoutsPage>
-    <UIModal v-model="showModal" title="新增tag" open-title=" Modal">
-      <template #default>
-        <UButton label="新增tag" color="neutral" variant="subtle" />
-      </template>
-      <template #body>
-        <UInput :model-value="props.addTagValue" @update:model-value="(_value: string) => emit('update:addTagValue', _value)" />
-      </template>
-      <template #footer>
-        <UButton type="button" color="primary" @click="create_tags">確認</UButton>
-      </template>
-    </UIModal>
+    <AddTagModal v-model:add-tag-value="addTagValue" @create-tags="createTags" />
     <UForm :state="props.form" :schema="schema" @submit="emit('form-submit')">
       <UFormField label="狀態" name="tag">
-        <UInputMenu
-          :model-value="props.form.tag" :items="props.tagItems" value-key="uuid"
-          @update:model-value="(_value: object)  => emit('update:form', { ...props.form, tag: _value})" />
+        <UInputMenu :model-value="props.form.tag" :items="props.tagItems" value-key="uuid"
+          @update:model-value="(_value: object) => emit('update:form', { ...props.form, tag: _value })" />
       </UFormField>
       <UFormField label="標題" name="title">
-        <UInput 
-          :model-value="props.form.title"
-          @update:model-value="(_value: string) => emit('update:form', { ...props.form, title: _value })" 
-        />
+        <UInput :model-value="props.form.title"
+          @update:model-value="(_value: string) => emit('update:form', { ...props.form, title: _value })" />
       </UFormField>
       <UFormField label="內容" name="content">
         <client-only>
-          <TiptapEditor 
-            :model-value="props.form.content"
+          <TiptapEditor :model-value="props.form.content"
             @update:model-value="(_value: string) => emit('update:form', { ...props.form, content: _value })" />
         </client-only>
       </UFormField>
