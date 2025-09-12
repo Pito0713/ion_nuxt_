f8c92fc104de5251a53e9af93d718cc6324e7014
Liang Aoi <58981867+Pito0713@users.noreply.github.com>
2025-09-12 17:09:20 +0800

    新增 eidtTag 頁面

---
+++ pages/editTag/UI.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { useHook } from './useHook'
+const { tagInfo } = useHook()
+console.log(tagInfo)
+import Modal from './Modal.vue';
+</script>
+
+<template>
+  <div class="mt-10">
+    <ul>
+      <li v-for="tag in tagInfo" :key="tag.uuid" class="py-6">
+        <UCard>
+          <template #header>
+            <a>
+              {{ tag.label }}
+            </a>
+          </template>
+          <div class="w-full flex flex-col md:flex-row gap-4">
+            <Modal :tag-info="tag" />
+          </div>
+        </UCard>
+      </li>
+    </ul>
+  </div>
+</template>
